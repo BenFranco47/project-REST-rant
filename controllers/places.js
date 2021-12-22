bf45a271975df09342d6c754009dd8d0b99dd981
Ben Franco <88692753+BenFranco47@users.noreply.github.com>
2021-12-22 16:36:37 -0500

    Adding remaining stub routes

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,4 @@
-const router = require('express').Router()
+const router = require('express').Router();
 
 // More code here in a moment
 router.get('/', (req, res) => {
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.use('/places', require('./controllers/places'))
 
